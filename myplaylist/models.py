7377c9cf248be3d46d18e7ecb4c7ee44b58e9947
Simo Paraschi <58208573+simoparaschi@users.noreply.github.com>
2025-08-31 17:11:46 +0200

    Added models

--- myplaylist/models.py
+++ myplaylist/models.py
@@ -3,3 +3,54 @@ from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class PlaylistSpotify(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    spotify_url = models.CharField()
+
+    def __str__(self):
+        return f"playlist {self.name}"
+
+class Track(models.Model):
+    name = models.CharField(max_length=50)
+    artist = models.CharField(max_length=50)
+    album = models.CharField(max_length=50)
+    spotify_url = models.CharField()
+
+    def __str__(self):
+        return f"Track {self.name}"
+
+# JOIN table
+class PlaylistSearched(models.Model):
+    playlist_spotify = models.ForeignKey(PlaylistSpotify, related_name="playlist_spotify")
+    track = models.ForeignKey(Track, on_delete=models.CASCADE, related_name="track_spotify")
+
+
+
+class Recco(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="searcher")
+    playlist_spotify = models.ForeignKey(PlaylistSpotify, related_name="playlist_spotify")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Recco {self.name}"
+
+
+
+# JOIN table
+class ReccoTracks(models.Model):
+    track = models.ForeignKey(Track, on_delete=models.CASCADE, related_name="track_spotify")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="searcher")
+
+
+# JOIN table
+class Favourite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="searcher")
+    recco_tracks = models.ForeignKey(ReccoTracks, on_delete=models.CASCADE, related_name="reccomended_tracks")
