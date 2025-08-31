ee57d6e826a9731d07b3d2b559c1a0a4ddcf4fad
Simo Paraschi <58208573+simoparaschi@users.noreply.github.com>
2025-08-31 18:23:56 +0200

    Fixed issue with secret exposed

--- recco/settings.py
+++ recco/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
+import os
+
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d236yfjqgfh60e50-u9(d3$#-bt-z1-(^j(9$vmgh@rs!@!%!j'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
