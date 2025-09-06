fdaf15f6d1e20c1c22583ce03f3209ab8a576130
Simo Paraschi <58208573+simoparaschi@users.noreply.github.com>
2025-09-06 10:35:41 +0200

    Request User Authorization works

--- myplaylist/services/spotify.py
+++ myplaylist/services/spotify.py
@@ -2,19 +2,53 @@ import os
 from dotenv import load_dotenv, find_dotenv
 import base64
 import requests
-import json
+from urllib.parse import urlencode
+
 
 # get Spotify client id and secret safely
 load_dotenv(find_dotenv())
 
-client_id = os.environ['CLIENT_ID']
-client_secret = os.environ['CLIENT_SECRET']
+CLIENT_ID = os.environ['CLIENT_ID']
+CLIENT_SECRET = os.environ['CLIENT_SECRET']
 
-def test_spotify():
-    return f"{client_id } - {client_secret }"
 
+
+# Request Spotify authorization
+# Using Authorization Code Flow
+# https://developer.spotify.com/documentation/web-api/tutorials/code-flow
+
+# Request User Authorization
+def login_spotify():
+    scope = "user-read-private user-read-email"
+    auth_url = "https://accounts.spotify.com/authorize?"
+    redirect_uri = "http://127.0.0.1:8000/callback"
+
+
+    params = {
+        "client_id": CLIENT_ID,
+        "response_type": "code",
+        "scope": scope,
+        "redirect_uri": redirect_uri,
+        "show_dialog": True
+    }
+    test = f"{auth_url}{urlencode(params)}"
+    print("***********************************")
+    print(test)
+    return f"{auth_url}{urlencode(params)}"
+
+
+
+# Request Access Token
+def callback():
+    pass
+    # body {
+    #     "code": request.GET.get("code")
+    # }
+
+
+# Old code
 def get_token():
-    auth_string = f"{client_id }:{client_secret}"
+    auth_string = f"{CLIENT_ID }:{CLIENT_SECRET}"
     auth_bytes = auth_string.encode("utf-8")
     auth_base64 = str(base64.b64encode(auth_bytes).decode("utf-8"))
 
@@ -28,5 +62,15 @@ def get_token():
 
     response = requests.post(url, headers=headers, data=payload)
     data = response.json()
-    print(data)
-    return data
+
+    # Get the access token specifically
+    token = data["access_token"]
+    return token
+
+
+def get_auth_header(token):
+    return {"Authorization": f"Bearer {token}"}
+
+def get_playlist(token):
+    url = "https://api.spotify.com/v1/users/"
+    headers = get_auth_header(token)
--- myplaylist/views.py
+++ myplaylist/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from myplaylist.services.spotify import test_spotify, get_token
+from myplaylist.services.spotify import get_token, login_spotify
 
 
 # Create your views here.
 def hello(request):
     test = get_token()
+    spotify_url = login_spotify()
     return render(request, "myplaylist/index.html",{
-        "test": test
+        "test": test,
+        "spotify_url": spotify_url
     })
