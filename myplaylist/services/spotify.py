47b2885b58f672182d1e84d1bccd492006f07e55
Simo Paraschi <58208573+simoparaschi@users.noreply.github.com>
2025-09-20 12:27:57 +0200

    moved exception to views to handle user error

--- myplaylist/services/spotify.py
+++ myplaylist/services/spotify.py
@@ -100,12 +100,6 @@ def get_playlist(token):
 
 
 def get_playlist_id(url):
-    try:
-        # Match the regex and extract the playlist ID
-        matches = re.search(r'playlist/(\w*).*', url)
-        return matches.group(1)  # This will raise an AttributeError if matches is None
-
-
-    except Exception as e:
-        message = "Invalid Spotify URL format. Could not extract track ID." if isinstance(e, AttributeError) else "An error occurred while processing the input:"
-        raise CleanError(f"{message}: {e}")
+    # Match the regex and extract the playlist ID
+    matches = re.search(r'playlist/(\w*).*', url)
+    return matches.group(1)  # This will raise an AttributeError if matches is None
--- myplaylist/views.py
+++ myplaylist/views.py
@@ -49,11 +49,13 @@ def dashboard(request):
     for item in playlists["items"]:
         list_playlist.append(item["name"])
         spotify_url = item["external_urls"]["spotify"]
-        spotify_id = get_playlist_id(spotify_url)
+        try:
+            spotify_id = get_playlist_id(spotify_url)
+        except Exception as e:
+            message = "Invalid Spotify URL format. Could not extract track ID." if isinstance(e, AttributeError) else "An error occurred while processing the input:"
+            raise CleanError(f"{message}: {e}")
+
         PlaylistSpotify.objects.get_or_create(name=item["name"], spotify_id=spotify_id, spotify_url=spotify_url, user=request.user)
-
-
-
 
     return render(request, "myplaylist/dashboard.html",{
         "list_playlist": list_playlist
