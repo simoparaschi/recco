7da5ef1659e591a6ef07639afec61729fea33652
Simo Paraschi <58208573+simoparaschi@users.noreply.github.com>
2025-09-20 17:48:00 +0200

    created playlist service

--- myplaylist/views.py
+++ myplaylist/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render, redirect
-from myplaylist.services.spotify import login_spotify, get_token, get_playlist, spotify_refresh_token, get_playlist_id
+from myplaylist.services.spotify import login_spotify, get_token, get_playlist, spotify_refresh_token, get_playlist_id # check all of these, long
 from myplaylist.services.session import save_access_tokens, get_user_tokens, is_expired,update_tokens
+from myplaylist.services.playlist import sync_playlist
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
 from .models import PlaylistSpotify
 
 
-
-# Raised when an regex input validation error occurs
-class CleanError(Exception):
-    pass
 
 
 # Create your views here.
@@ -43,22 +40,13 @@ def dashboard(request):
         update_tokens(request, new_tokens)
 
 
-
     # Get user's playlists
     playlists = get_playlist(tokens["access_token"])
 
-    # Display user's playlists
-    list_playlist = []
-    for item in playlists["items"]:
-        list_playlist.append(item["name"])
-        spotify_url = item["external_urls"]["spotify"]
-        try:
-            spotify_id = get_playlist_id(spotify_url)
-        except Exception as e:
-            message = "Invalid Spotify URL format. Could not extract track ID." if isinstance(e, AttributeError) else "An error occurred while processing the input:"
-            raise CleanError(f"{message}: {e}")
 
-        PlaylistSpotify.objects.get_or_create(name=item["name"], spotify_id=spotify_id, spotify_url=spotify_url, user=request.user)
+    # Display user's playlists
+    list_playlist = sync_playlist(playlists, request)
+
 
     return render(request, "myplaylist/dashboard.html",{
         "list_playlist": list_playlist
